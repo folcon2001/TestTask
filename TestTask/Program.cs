5764c9716be6abfbda249d684c00979ea97df3f6
222 111 <folcon2001@gmail.com>
2021-04-07 22:26:03 +0300

    проверка исключений

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -14,25 +14,25 @@ namespace Countwords
         public static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            string adr, pathcont, pathres;
+            string adr, pathcont, pathres,resdownload;
 
 
-            Console.Title = "Ввод информации";
-            Console.WriteLine("\nВведите адрес сайта в формате: http://xxxxx.xx \n");
-            adr = Console.ReadLine();
-
-
-
-            if (adr.Length < 10 || adr.Substring(0,7)!="http://" || !adr.Contains("."))
-            {
-                do
-                {
-                    Console.Title = "Ошибка ввода";
-                    Console.WriteLine("Не верный формат ввода адреса!!! Попробуйте еще раз.");
-                    adr = Console.ReadLine();
-
-                } while (adr.Length < 10 || adr.Substring(0, 7) != "http://" || !adr.Contains(".") );
-            }
+           Console.Title = "Ввод информации";
+           Console.WriteLine("\nВведите адрес сайта в формате: http://xxxxx.xx \n");
+           adr = Console.ReadLine();
+         
+         
+         
+           if (adr.Length < 10 || adr.Substring(0,7)!="http://" || !adr.Contains("."))
+           {
+               do
+               {
+                   Console.Title = "Ошибка ввода";
+                   Console.WriteLine("Не верный формат ввода адреса!!! Попробуйте еще раз.");
+                   adr = Console.ReadLine();
+         
+               } while (adr.Length < 10 || adr.Substring(0, 7) != "http://" || !adr.Contains(".") );
+           }
 
 
             // путь  сохранения файла содержимого сайта
@@ -44,15 +44,18 @@ namespace Countwords
             logger.Trace("Адрес сайта получен, пути для сохранения файлов определены");
 
             Download zagruz = new Download();
-            zagruz.Web(adr, pathcont);
+            resdownload=zagruz.Web(adr, pathcont);
+            if (resdownload == "")
+            {
+                Sorting otbor = new Sorting();
+                otbor.Count(pathcont, pathres);
 
-            Sorting otbor = new Sorting();
-            otbor.Count(pathcont, pathres);
+                Baza import = new Baza();
+                import.Import(adr, pathres);
 
-            Baza import = new Baza();
-            import.Import(adr, pathres);
+                logger.Trace("Программа успешно завершена.");
+            }
 
-            logger.Trace("Программа успешно завершена.");
         }
     }
 
@@ -63,7 +66,7 @@ namespace Countwords
         /* Метод получает адрес сайта и путь к файлу для сохранения.
          * Скачивает содержимое сайта и сохраняет в файл.  */
 
-        public void Web(string adr, string path)
+        public string Web(string adr, string path)
         {
             try
             {
@@ -84,14 +87,17 @@ namespace Countwords
                 logger.Trace("Веб страница загружена и сохранена в файл");
 
                 Console.WriteLine("\nВеб страница загружена!\n");
+                return "";
             }
 
             catch (Exception er)
             {
                 Console.Title = "Ошибка ввода";
-                Console.WriteLine("Ошибка подключения! {0}", er.Message);
+                Console.WriteLine($"\nОшибка подключения! \n{er.Message}\n");
                 logger.Error("Ошибка подключения к сайту");
+                return er.Message;
             }
+            
         }
     }
 
@@ -110,77 +116,85 @@ namespace Countwords
 
             int n = 0;
 
-            txt = File.ReadAllText(pathc).ToUpper();
-
-            logger.Trace("Данные считаны из файла");
-
-            Console.WriteLine("\nИдет обработка данных.\n");
-
-            string[] subs = txt.Split(new char[] { ' ', '{', '}', '(', ')', '?', '!', '\\', '<', '-', ';', ':', '/', ',', '"', '=', '>', '<', '+', '.', '&', '%', '_', '\'', '|', '[', ']', '$', '*', '#', '@', '~', '^', '«', '»', '—', '–', '−', '…', '°', '\n', '\r', '\'', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-
-
-            //убираем из массива "слова" содержащие не только буквы
-
-            for (int i = 0; i < subs.Length; i++)
+            try
             {
-                for (int j = 0; j < subs[i].Length; j++)
+                txt = File.ReadAllText(pathc).ToUpper();
+
+                logger.Trace("Данные считаны из файла");
+
+                Console.WriteLine("\nИдет обработка данных.\n");
+
+                string[] subs = txt.Split(new char[] { ' ', '{', '}', '(', ')', '?', '!', '\\', '<', '-', ';', ':', '/', ',', '"', '=', '>', '<', '+', '.', '&', '%', '_', '\'', '|', '[', ']', '$', '*', '#', '@', '~', '^', '«', '»', '—', '–', '−', '…', '°', '\n', '\r', '\'', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                //убираем из массива "слова" содержащие не только буквы
+
+                for (int i = 0; i < subs.Length; i++)
                 {
-                    if (!char.IsLetter(subs[i][j]))
+                    for (int j = 0; j < subs[i].Length; j++)
                     {
-                        n = 1;
-                        break;
+                        if (!char.IsLetter(subs[i][j]))
+                        {
+                            n = 1;
+                            break;
+                        }
+
+
+                    }
+                    if (n == 0)
+                        num += subs[i] + " ";
+
+                    if (i % 1000 == 0)
+                        Console.Write(".");
+
+                    n = 0;
+                }
+
+                logger.Trace("Из массива убраны слова содержащие не только буквы");
+
+                string[] obch = num.Split();
+
+                string[] izb = obch.Distinct().ToArray();
+
+                for (int i = 0; i < izb.Length; i++)
+                {
+                    for (int j = 0; j < obch.Length; j++)
+                    {
+                        if (izb[i] != "" && izb[i] == obch[j])
+                            n++;
+
                     }
 
+                    if (n != 0)
+                        izb[i] = izb[i] + " " + n;
 
+                    n = 0;
                 }
-                if (n == 0)
-                    num += subs[i] + " ";
 
-                if (i % 1000 == 0)
-                    Console.Write(".");
+                Array.Sort(izb);
 
-                n = 0;
-            }
+                logger.Trace("Подсчет уникальных слов выполнен");
 
-            logger.Trace("Из массива убраны слова содержащие не только буквы");
+                Console.WriteLine("\n");
 
-            string[] obch = num.Split();
-
-            string[] izb = obch.Distinct().ToArray();
-
-            for (int i = 0; i < izb.Length; i++)
-            {
-                for (int j = 0; j < obch.Length; j++)
+                foreach (string s in izb)
                 {
-                    if (izb[i] != "" && izb[i] == obch[j])
-                        n++;
-
+                    Console.WriteLine(s + "\n");
+                    if (s != "")
+                        itog += s + "\n";
                 }
 
-                if (n != 0)
-                    izb[i] = izb[i] + " " + n;
+                File.WriteAllText(pathr, itog);
 
-                n = 0;
+                Console.WriteLine("\nДанные обработаны!\n");
+
+                logger.Trace("Результат обработки сохранен в файле");
             }
-
-            Array.Sort(izb);
-
-            logger.Trace("Подсчет уникальных слов выполнен");
-
-            Console.WriteLine("\n");
-
-            foreach (string s in izb)
+            catch
             {
-                Console.WriteLine(s + "\n");
-                if (s != "")
-                    itog += s + "\n";
+                Console.WriteLine("\nНе найден файл \"contains.txt\" с содержимым сайта!\n");
+                logger.Error("Не найден файл contains.txt");
             }
-
-            File.WriteAllText(pathr, itog);
-
-            Console.WriteLine("\nДанные обработаны!\n");
-
-            logger.Trace("Результат обработки сохранен в файле");
         }
     }
 
@@ -203,60 +217,69 @@ namespace Countwords
             adrs = name.Substring(7);
             poz = adrs.IndexOf('.');
             a = adrs.Substring(0, poz);
-
-            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["Baza"].ConnectionString);
-           
-            Console.WriteLine("Проверка входа");
-
-            myConn.Open();
-
-            if (myConn.State == ConnectionState.Open)
-            {
-                Console.WriteLine("Подключение к базе данных выполнено успешно!");
-                logger.Trace("Подключение к базе данных выполнено успешно");
-            }
-
-            SqlCommand myCommand = new SqlCommand("CREATE DATABASE MyDatabase", myConn);
-
-            string query = "CREATE TABLE " + a + "(slovo NVARCHAR(40), povtor INT)";
-
-            SqlCommand sqlNewTab = new SqlCommand(query, myConn);
             try
             {
-                sqlNewTab.ExecuteNonQuery();
+                SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["Baza"].ConnectionString);
 
-                Console.WriteLine($"\nТаблица создана c именем \"{a}\"");
-                logger.Trace("Создана новая таблица с названием сайта");
+                myConn.Open();
+
+
+                if (myConn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Подключение к базе данных выполнено успешно!");
+                    logger.Trace("Подключение к базе данных выполнено успешно");
+                }
+
+                SqlCommand myCommand = new SqlCommand("CREATE DATABASE MyDatabase", myConn);
+
+                string queryDel = "DROP TABLE " + a ;
+
+                SqlCommand sqlDelTab = new SqlCommand(queryDel, myConn);
+
+                sqlDelTab.ExecuteNonQuery();
+
+                string query = "CREATE TABLE " + a + "(slovo NVARCHAR(40), povtor INT)";
+
+                SqlCommand sqlNewTab = new SqlCommand(query, myConn);
+               
+                    sqlNewTab.ExecuteNonQuery();
+
+                    Console.WriteLine($"\nВ базе данных создана таблица c именем \"{a}\"");
+                    logger.Trace("Создана новая таблица с названием сайта");
+                
+                
+
+               
+                    StreamReader file = new StreamReader(path);
+
+                    Console.WriteLine("\nИмпортируем данные в базу данных.\n");
+
+                    while ((txt = file.ReadLine()) != null)
+                    {
+                        if (txt != "")
+                        {
+                            string[] para = txt.Split();
+
+                            query = "INSERT INTO " + a + " (slovo, povtor) VALUES ('" + para[0] + "','" + Int32.Parse(para[1]) + "')";
+                            SqlCommand sqlNewString = new SqlCommand(query, myConn);
+                            sqlNewString.ExecuteNonQuery();
+                        }
+                    }
+
+                    file.Close();
+               
+
+                myConn.Close();
+
+                logger.Trace("Данные импортированы в базу данных");
+
+                Console.WriteLine("\nДанные успешно импортированы в базу данных!\n");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
             }
-
-            StreamReader file = new StreamReader(path);
-
-            Console.WriteLine("\nИмпортируем данные в базу данных.\n");
-
-            while ((txt = file.ReadLine()) != null)
-            {
-                if (txt != "")
-                {
-                    string[] para = txt.Split();
-
-                    query = "INSERT INTO " + a + " (slovo, povtor) VALUES ('" + para[0] + "','" + Int32.Parse(para[1]) + "')";
-                    SqlCommand sqlNewString = new SqlCommand(query, myConn);
-                    sqlNewString.ExecuteNonQuery();
-                }
-            }
-
-            file.Close();
-
-            myConn.Close();
-
-            logger.Trace("Данные импортированы в базу данных");
-
-            Console.WriteLine("\nДанные успешно импортированы в базу данных!\n");
-
         }
     }
 }
